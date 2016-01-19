6a96cf99cfc44b209578cd1bb517e8d26a90e109
Mohammed Rizvi <morizvi07@gmail.com>
2016-01-18 23:38:44 -0500

    removed original bootstrap theme and replaced with materialize added header and navbar

---
+++ js/exercise1.js
@@ -0,0 +1,7 @@
+var argv = process.argv;
+
+if argv([2]) === argv([3]) 
+  console,log(true)
+} else {
+  console.log(false)
+}
---
+++ js/script.js
@@ -0,0 +1,4 @@
+$(document).ready(function(){
+  $('.slider').slider();
+  $(".button-collapse").sideNav();
+});
